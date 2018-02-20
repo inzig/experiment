40aa3d183ab9848d3a86f45afda76473dac8db07
inzamam.hassan@iqvis.com <inzamam.hassan@iqvis.com>
2018-02-20 15:45:48 +0500

    develop branch

---
+++ routes/index.js
@@ -0,0 +1,223 @@
+var express = require('express');
+var router = express.Router();
+var Web3 = require('web3');
+
+if (typeof web3 !== 'undefined') {
+  web3 = new Web3(web3.currentProvider);
+} else {
+  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+}
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/getTransaction', function(req, res, next) {  
+  var t = web3.eth.getTransaction(req.body.transaction);
+  res.send(t);
+});
+
+var account = web3.personal.listAccounts[0];
+
+router.post('/transfer', function(req, res, next) {  
+  var Iotide = authorize();  
+  var response = Iotide.transfer.sendTransaction (req.body.address, req.body.amount, {
+                  from: account,
+                  gas:4000000 }, function (error, result){                       
+                      if(!error){                         
+                          res.send(result);
+                      } else{
+                          console.log(error);
+                          res.send(error);
+                      }
+                  });
+});
+
+router.post('/balance', function(req, res, next) {
+  var Iotide = authorize(); 
+  var response = Iotide.balanceOf( account, {
+                  from: account,
+                  gas:4000000 }, function (error, result){ 
+                      if(!error){
+                          // console.log(result);
+                          res.send(result);
+                      } else{
+                          // console.log(error);
+                          res.send('error');
+                      }
+                  });      
+});
+
+function authorize(){
+  web3.personal.unlockAccount(account,'123456789', 300);
+  web3.eth.defaultAccount = account;
+  return web3.eth.contract([
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_spender",
+          "type": "address"
+        },
+        {
+          "name": "_value",
+          "type": "uint256"
+        }
+      ],
+      "name": "approve",
+      "outputs": [
+        {
+          "name": "success",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_from",
+          "type": "address"
+        },
+        {
+          "name": "_to",
+          "type": "address"
+        },
+        {
+          "name": "_value",
+          "type": "uint256"
+        }
+      ],
+      "name": "transferFrom",
+      "outputs": [
+        {
+          "name": "success",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "_owner",
+          "type": "address"
+        }
+      ],
+      "name": "balanceOf",
+      "outputs": [
+        {
+          "name": "balance",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_to",
+          "type": "address"
+        },
+        {
+          "name": "_value",
+          "type": "uint256"
+        }
+      ],
+      "name": "transfer",
+      "outputs": [
+        {
+          "name": "success",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "_owner",
+          "type": "address"
+        },
+        {
+          "name": "_spender",
+          "type": "address"
+        }
+      ],
+      "name": "allowance",
+      "outputs": [
+        {
+          "name": "remaining",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "_from",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "name": "_to",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "name": "_value",
+          "type": "uint256"
+        }
+      ],
+      "name": "Transfer",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "_owner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "name": "_spender",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "name": "_value",
+          "type": "uint256"
+        }
+      ],
+      "name": "Approval",
+      "type": "event"
+    }
+  ]).at('0x1f7db4af4df9c0561f18a38a7a697059d41422c6');
+}
+
+module.exports = router;
